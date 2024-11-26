c4ec4fddc600cffe8f0ad8f71b9557d35c1b1614
dlskfk <naralee95@gmail.com>
2024-11-26 11:41:12 +0900

    docker Practice

---
+++ rest-api-server/src/main/java/com/ohgiraffers/jwtrestapi/member/repository/MemberRoleRepository.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.jwtrestapi.member.repository;
+
+import com.ohgiraffers.jwtrestapi.member.entity.MemberRole;
+import com.ohgiraffers.jwtrestapi.member.entity.MemberRolePk;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRoleRepository extends JpaRepository<MemberRole, MemberRolePk> {
+
+
+}
